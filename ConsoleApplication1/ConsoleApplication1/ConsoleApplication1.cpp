4ab6268f9c5bdb55c9471c42765c92c57f34fba0
xpe1l <semenuk.boghdan@gmail.com>
2022-12-13 23:09:10 +0200

    laboratorna-2-

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿
+// програма виводить додавання, віднімання, множення, ділення двох чисел
+
+#include <cstdio>;
+#include <iostream>;
+using namespace std;
+int main() {
+	double a, b, c, d, e, f;
+
+	cout << "Enter 1 number:";
+	cin >> a;
+	cout << "Enter 2 number:";
+	cin >> b;
+
+	c = a + b;
+	d = a - b;
+	e = a * b;
+	f = a / b;
+
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	cout << f << endl;
+}
